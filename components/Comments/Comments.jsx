035fad7585f88301d716166d5e299b607acb8aed
gladja <gladkih.alexander@gmail.com>
2023-11-03 20:44:37 +0200

    add rtk query

---
+++ components/Comments/Comments.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Comment } from '../Comment/Comment';
+import { Grid } from '../Grid/Grid';
+import { comments } from '../../helpers/comments';
+import {useGetCommentsQuery} from '../../redux/commentApi.js'
+
+export const Comments = () => {
+  const obj = useGetCommentsQuery();
+
+  console.log(useGetCommentsQuery)
+
+  return (
+    <Grid>
+      {comments &&
+        comments.map((comment) => <Comment key={comment.id} {...comment} />)}
+    </Grid>
+  );
+};
+
+Comments.propTypes = {
+  comments: PropTypes.arrayOf(PropTypes.shape().isRequired),
+};
---
+++ redux/commentApi.js
@@ -0,0 +1,23 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+
+const API_ENDPOINT = '/Comments';
+const BASE_URL = 'https://65284718931d71583df21c39.mockapi.io/';
+
+export const commentApi = createApi({
+    reducerPath: 'comments',
+    baseQuery: fetchBaseQuery({
+        baseUrl: BASE_URL,
+    }),
+    tagTypes: ['Comments'],
+    endpoints: (builder) => ({
+        getComments: builder.query({
+            query: () => API_ENDPOINT,
+            providesTags: ['Comments'],
+        })
+    }),
+});
+
+export const { useGetCommentsQuery } = commentApi;
+
+
